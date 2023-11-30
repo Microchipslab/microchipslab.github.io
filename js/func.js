d3798e79403f5ea09ea2d03a9cd8778bf69109a7
Microchipslab <151474464+Microchipslab@users.noreply.github.com>
2023-11-30 21:49:49 +0000

    Script added

--- js/func.js
+++ js/func.js
@@ -26,5 +26,16 @@ function includeHTML() {
   }
 }
 
-
-
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
